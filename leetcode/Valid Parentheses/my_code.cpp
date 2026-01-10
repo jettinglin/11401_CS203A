f01e90e82c5148156b3e922b807a2acdd8f1d113
jet ting lin <jet.lin.0304@gmail.com>
2026-01-10 18:26:43 +0800

    Create my_code.cpp

---
+++ leetcode/Valid Parentheses/my_code.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size()%2!=0)
+        return false;
+        stack<char> st;
+        for(int i = 0;i<s.size();i++)
+        {
+            if(st.empty()||s[i]=='('||s[i]=='{'||s[i]=='[')
+            st.push(s[i]);
+            else if(s[i]==')'&&st.top()=='('||s[i]==']'&&st.top()=='['||s[i]=='}'&&st.top()=='{')
+            st.pop();
+            else 
+            return false;
+        }
+        return st.empty();
+    }
+};
